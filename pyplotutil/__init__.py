05d7c6dae45b5e8c2c6a5beb3d2083b8b552c7c9
Hiroshi Atsuta <atsuta.hiroshi@gmail.com>
2018-02-25 21:23:46 +0100

    Add first test

---
+++ pyplotutil/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+    pyplotutil
+    ~~~~~~~~~~
+    A plotting utility based on matplotlib.
+    :copyright: © 2018 by Hiroshi Atusta.
+    :license: MIT, see LICENSE for more details.
+"""
+
+__version__ = '0.1.0'
